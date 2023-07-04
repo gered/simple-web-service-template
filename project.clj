f2584d9013ae8db427d80c1bb3d374832f8c63d9
gered <gered@blarg.ca>
2023-07-04 11:53:31 -0400

    bump to next snapshot version for future development

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject net.gered/lein-template.simple-web-service "0.3.1"
+(defproject net.gered/lein-template.simple-web-service "0.3.2-SNAPSHOT"
   :description         "Simple Clojure web service project template."
   :url                 "https://github.com/gered/simple-web-service-template"
   :license             {:name "MIT License"
